a49cfadf17ecbbf25ecfdc47ad23c1cffa60839e
manodharma <mayaaskumar@gmail.com>
2022-10-25 20:12:14 +0700

    free number and changed choices

--- leads/models.py
+++ leads/models.py
@@ -30,7 +30,7 @@ class LeadManager(models.Manager):
 
 
 class Lead(models.Model):
-    CHOICES = [(i+1,i+1) for i in range(10)]
+    CHOICES = [(str(i+1),str(i+1)) for i in range(10)]
     phone_number = models.OneToOneField("Number", unique=True, on_delete=models.PROTECT, verbose_name='Номер телефона')    
     mac_address = models.CharField(max_length=12,blank=True, verbose_name='MAC-Адрес', validators = [
         RegexValidator(
