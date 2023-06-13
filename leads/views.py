e7c527ba1f82d5fdc931390f5d7035e3d62df00a
araimbekov <raim.azim@mail.ru>
2023-06-13 10:44:42 +0800

    added extra info

--- leads/views.py
+++ leads/views.py
@@ -298,9 +298,9 @@ def lead_list(request):
 def lead_detail(request, pk):
     lead = Lead.objects.get(id=pk)
     phone = str(lead.phone_number)
-    # res = requests.get('http://10.90.42.250:8084/phoneinfo?phone=' + phone)
-    # res_json = res.json()
-    res_json = {'phone': '2252', 'status': 'idle', 'ipaddr': '10.2.3.4', 'socketinfo': {'port': 'Gi3/0/41', 'cabinet': '721', 'socket': '715.22', 'discription': '16.03.20_ALFA_721_715.22', 'ipaddr': '10.2.3.40'}}
+    res = requests.get('http://10.90.42.250:8084/phoneinfo?phone=' + phone)
+    res_json = res.json()
+    # res_json = {'phone': '2252', 'status': 'idle', 'ipaddr': '10.2.3.4', 'socketinfo': {'port': 'Gi3/0/41', 'cabinet': '721', 'socket': '715.22', 'discription': '16.03.20_ALFA_721_715.22', 'ipaddr': '10.2.3.40'}}
     
     number_api = res_json['phone']
     atc_ip = res_json['ipaddr']
